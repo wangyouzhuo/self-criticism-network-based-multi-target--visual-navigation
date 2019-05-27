8b475452d8142c8fd6cee0f4b58f425bbe87a7a4
wyz <459239181@qq.om>
2019-05-27 19:15:11 +0800

    init

---
+++ config/constant.py
@@ -0,0 +1,22 @@
+
+ROOT_PATH = '/home/wyz/PycharmProjects/distill_based_visual_navigation_in_indoor/'
+
+GAMMA                = 0.95
+
+MAX_STEP_IN_EVALUATE = 1000
+MAX_STEP_IN_EPISODE  = 1000
+
+MAX_GLOBAL_EP        = 100000
+
+UPDATE_SPECIAL_ITER  = 10
+UPDATE_GLOBAL_ITER   = 10
+
+EVALUATE_ITER        = 100
+
+N_EVALUATERS         = 20
+N_WORKERS            = 40
+
+device = '/gpu:0'
+
+
+
---
+++ config/params.py
@@ -0,0 +1,20 @@
+LR_A = 0.0001  # learning rate for actor
+LR_C = 0.0001  # learning rate for critic
+
+LR_REG_A = 0.0001
+LR_REG_C = 0.0001
+
+ENTROPY_BETA = 0.1
+
+TARGET_ID_LIST =   [
+                      113, 56, 265, 311, 23 ,
+                      66 , 245,123, 5  , 99 ,
+                      333, 59, 400,19 , 377,
+                      77 , 333,257,1  , 283,
+                      355, 100,79, 200, 2
+                   ]
+
+
+BETA_REG_VALUE = 0.000
+
+BETA_REG_ACTION = 0.01
